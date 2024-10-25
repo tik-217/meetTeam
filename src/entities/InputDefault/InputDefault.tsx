629256b1a7a4ddaaae9df50151c3ea4a7a3122da
gabulyan_t_qschool <tigran.gabulyan.2001@mail.ru>
2024-10-25 08:21:56 +0300

    fix: generate inputs id

--- src/entities/InputDefault/InputDefault.tsx
+++ src/entities/InputDefault/InputDefault.tsx
@@ -1,20 +1,23 @@
 import { IInputDefaultProps } from "@/shared/types/InputTypes";
 import "./InputDefault.scss";
+import { v4 as uuidv4 } from "uuid";
 
 export const InputDefault = ({
   labelText,
   type,
   placeholder,
 }: Partial<IInputDefaultProps>) => {
+  const inputId = uuidv4();
+
   return (
     <div className="inputDefault">
-      <label className="inputDefault__label" htmlFor="inputDefaultId">
+      <label className="inputDefault__label" htmlFor={inputId}>
         {labelText}
       </label>
       <input
         className="inputDefault__input"
         type={type}
-        id="inputDefaultId"
+        id={inputId}
         placeholder={placeholder}
       />
     </div>
