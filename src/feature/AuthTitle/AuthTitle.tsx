e4a82893d997f6db4de948e0addc37e2a5e05f23
gabulyan_t_qschool <tigran.gabulyan.2001@mail.ru>
2024-10-25 09:54:13 +0300

    fix: fixing title of a page ForgetPass

--- src/feature/AuthTitle/AuthTitle.tsx
+++ src/feature/AuthTitle/AuthTitle.tsx
@@ -4,11 +4,16 @@ import logo from "@/shared/images/logo.svg";
 export const AuthTitle = ({
   withoutDescription,
 }: {
-  withoutDescription: boolean;
+  withoutDescription?: boolean;
 }) => {
   return (
     <section className="authTitle__titleSection">
       <img src={logo} alt="logo" />
+      {!withoutDescription && (
+        <p className="authTitle__titleText">
+          Merci d'entrer vos informations de connexion
+        </p>
+      )}
       {!withoutDescription && (
         <p className="authTitle__titleText">
           Merci d'entrer vos informations de connexion
